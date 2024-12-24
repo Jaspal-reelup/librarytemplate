e0f71891e07d7074bf379df2cb66f63aad8cc544
Jaspal Singh <jaspalsingh@Jaspals-MacBook-Air-2.local>
2024-12-24 17:55:38 +0530

    init

---
+++ components/CustomDiv.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CustomDiv = () => {
+  return (
+    <View style={{ backgroundColor: 'blue', height: 100, width: 100 }}>
+      <Text>CustomDiv checking</Text>
+    </View>
+  )
+}
+
+export default CustomDiv
---
+++ components/index.ts
@@ -0,0 +1,5 @@
+import { Task } from "./Task";
+import { TaskList } from "./TaskList";
+import { LoadingRow } from "./LoadingRow";
+import CustomDiv from "./CustomDiv";
+export { Task, TaskList, LoadingRow, CustomDiv };
